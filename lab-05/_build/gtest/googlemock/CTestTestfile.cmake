03067a605e3c6fa4803840982c3f2c994e91cfb0
Yaros09 <91633974+Yaros09@users.noreply.github.com>
2022-05-14 15:56:26 +0300

    Delete lab-05/_build directory

--- lab-05/_build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/v_idro4k_a/lab05/third-party/gtest/googlemock
-# Build directory: /home/v_idro4k_a/lab05/_build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- lab-05/_build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/v_idro4k_a/lab05/third-party/gtest/googletest
-# Build directory: /home/v_idro4k_a/lab05/_build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
